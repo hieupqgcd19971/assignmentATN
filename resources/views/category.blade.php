df9b7c9732cddb75da77879682613a0f9d1a2117
hieupqgcd19971 <hieupqgcd19971@fpt.edu.vn>
2021-07-04 03:54:38 +0700

    af

--- resources/views/category.blade.php
+++ resources/views/category.blade.php
@@ -162,7 +162,7 @@
 
         <!-- Sidebar Message -->
         <div class="sidebar-card d-none d-lg-flex">
-            <img class="sidebar-card-illustration mb-2" src="img/undraw_rocket.svg" alt="...">
+        <img class="sidebar-card-illustration mb-2" src="{{asset('image/undraw_rocket.svg')}}" alt="...">
             <p class="text-center mb-2"><strong>SB Admin Pro</strong> is packed with premium features, components, and more!</p>
             <a class="btn btn-success btn-sm" href="">Upgrade to Pro!</a>
         </div>
