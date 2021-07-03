93807207c27fc66174d06c62c6451460c3d63dea
hieupqgcd19971 <hieupqgcd19971@fpt.edu.vn>
2021-07-04 02:18:25 +0700

    ppp

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -35,7 +35,7 @@ class HomeController extends Controller
             session('user',$username);
             return Redirect::to('atnpage');
         }else{
-            return Redirect::to('login');
+            return Redirect::to('');
         }
     }
     
