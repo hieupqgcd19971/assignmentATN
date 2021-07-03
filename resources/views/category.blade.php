fb75938abeb3c572fca059aa582405359f3ba480
hieupqgcd19971 <hieupqgcd19971@fpt.edu.vn>
2021-07-04 02:27:57 +0700

    hi

--- resources/views/category.blade.php
+++ resources/views/category.blade.php
@@ -146,7 +146,7 @@
 
         <!-- Nav Item - Tables -->
         <li class="nav-item">
-            <a class="nav-link" href="tables.html">
+            <a class="nav-link" href="{{URL::to('logout')}}">
                 <i class="fas fa-fw fa-table"></i>
                 <span>Đăng xuất</span></a>
         </li>
